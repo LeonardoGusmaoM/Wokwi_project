dc14b00128fa1b2104792350d3bb2283ee8a2f42
Leonardo Gusmão <leonardogusmaom@gmail.com>
2026-04-24 22:24:49 -0300

    adição de arquivos da v2

--- 2_leds_piscando.ino
+++ 2_leds_piscando.ino
@@ -26,6 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -33,5 +34,7 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(9,HIGH);
   delay(1000);                       // wait for a second
+  digitalWrite(9, LOW);
 }
